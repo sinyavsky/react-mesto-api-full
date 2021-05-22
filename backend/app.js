7341ef497b4ed0a554c956c7a5d28e4f2167665b
Alexander Sinyavsky <paul_norck@hotmail.com>
2021-05-22 13:27:25 +0300

    добавить cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const { login, createUser } = require('./controllers/users');
 const { validateLogin, validateCreateUser } = require('./middlewares/validators');
@@ -20,6 +21,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
 app.use(requestLogger);
