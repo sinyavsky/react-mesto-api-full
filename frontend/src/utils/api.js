1bcf5c69e6b479a27a03c398ba09f4ded7352d43
Alexander Sinyavsky <paul_norck@hotmail.com>
2021-05-22 17:08:34 +0300

    вернуть bearer

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,17 @@
 class Api {
 
-  constructor({baseUrl}) {
+  constructor({baseUrl, token}) {
     this.baseUrl = baseUrl;
+    this.token = token;
   }
 
   _makeRequest({method, path, contentType=null, body=null}) {     
     const options = {
       method: method,
-      mode: 'no-cors'
+      mode: 'no-cors',
+      headers: {
+        authorization: this.token
+      }
     };
 
     if(contentType) {
@@ -102,7 +106,8 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'https://api.mesto.sinyavsky.com'
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-20',
+  token: '8c32355f-4347-4868-8ec7-20db7d4995bd'
 }); 
 
 export default api;
